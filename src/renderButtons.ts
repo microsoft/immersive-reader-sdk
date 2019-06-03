e8183c92e10bae22c82fbae63555b9dc47319ec3
Meng Tan <48224849+metanMSFT@users.noreply.github.com>
2019-06-03 15:54:53 -0700

    Render launch buttons on window load

---
+++ src/renderButtons.ts
@@ -0,0 +1,195 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+const attributeIconPxSize = 'data-icon-px-size';
+const attributeButtonStyle = 'data-button-style';
+const attributeLocale = 'data-locale';
+
+const iconUrl: string = 'https://contentstorage.onenote.office.net/onenoteltir/permanent-static-resources/immersive-reader-icon.svg';
+
+const locs: { [key: string]: string} = {
+    af: 'Indompelende leser',
+    am: 'በህያው ስሜት ፈጣሪ ዕይታ አንባቢ',
+    ar: 'القارئ الشامل',
+    as: 'ইমাৰচিভ ৰিডাৰ',
+    az: 'İmmersiv Oxuyucu',
+    be: 'Імерсіўны чытальнік',
+    bg: 'Концентриран четец',
+    bn: 'মনোগ্রাহী পাঠক',
+    'bn-bd': 'ইমার্সিভ পাঠক',
+    bs: 'Koncentrirani čitalac',
+    ca: 'Lector immersiu',
+    cs: 'Asistivní čtečka',
+    'cy-gb': 'Darllenydd Ymdrwythol',
+    da: 'Forenklet læser',
+    de: 'Plastischer Reader',
+    el: 'Προηγμένο πρόγραμμα ανάγνωσης',
+    en: 'Immersive Reader',
+    es: 'Lector inmersivo',
+    et: 'Süvaluger',
+    eu: 'Irakurgailu murgiltzailea',
+    fa: 'خواننده همه‌جانبه',
+    fi: 'Syventävä lukuohjelma',
+    fil: 'Immersive Reader',
+    fr: 'Lecteur immersif',
+    'ga-ie': 'Léitheoir tumthach',
+    gd: 'An leughadair ùr-nòsach',
+    gl: 'Lector avanzado',
+    gu: 'ઇમર્સિવ રીડર',
+    ha: 'Mai karatu Mai barbazawa',
+    he: 'תצוגת קריאה מודרנית',
+    hi: 'इमर्सिव रीडर',
+    hr: 'Stopljeni čitač',
+    hu: 'Modern olvasó',
+    hy: 'Խորասուզված ընթերցիչ',
+    id: 'Pembaca Imersif',
+    ig: 'Ọgụụ Kenzipụtara',
+    is: 'Aðgengilegt lestrarumhverfi',
+    it: 'Strumento di lettura immersiva',
+    ja: 'イマーシブ リーダー',
+    ka: 'იმერსიული წამკითხველი',
+    kk: 'Иммерсивті оқу құралы',
+    km: 'កម្មវិធីអានពណ៌រំលេចនៅលើអេក្រង់',
+    kn: 'ಇಮ್ಮರ್ಸಿವ್ ಓದುಗ',
+    ko: '몰입형 리더',
+    kok: 'तंद्री लागिल्लो वाचक',
+    'ku-arab': 'خوێنەری پڕ',
+    ky: 'Курчаган Окугуч',
+    lb: 'Immersive Reader',
+    lo: 'ຕົວອ່ານອິມເມີສີບ',
+    lt: 'Įtraukianti skaitytuvė',
+    lv: 'Tīrskata lasītājs',
+    mi: 'Pūpānui Rumaki',
+    mk: 'Сеопфатен читач',
+    ml: 'ഇമ്മേഴ്‌സീവ് റീഡർ',
+    mn: 'Идэвхтэй уншигч',
+    mr: 'इमर्सिव्ह वाचक',
+    ms: 'Pembaca Imersif',
+    mt: 'Qarrej Immersiv',
+    ne: 'इमेर्सिभ रिडर',
+    nl: 'Insluitende lezer',
+    'nn-no': 'Engasjerande lesar',
+    no: 'Engasjerende leser',
+    nso: 'Go Bala ka Mongwalo o Mokoto',
+    or: 'ଇମରସିଭ୍ ରିଡର୍',
+    pa: 'ਇਮਰਸਿਵ ਰੀਡਰ',
+    'pa-arab': 'گھیرن آلا مطالعہ کار',
+    pl: 'Czytnik immersyjny',
+    prs: 'خواننده نمایش',
+    pt: 'Leitura Avançada',
+    quc: 'Nim ucholajil sik\'inel uwach',
+    quz: 'Wankisqa Ñawiq',
+    ro: 'Immersive Reader',
+    ru: 'Иммерсивное средство чтения',
+    rw: 'Insakazasoma',
+    sd: 'امرسو ريڊر',
+    si: 'ගිලෙන සුළු කියවනය',
+    sk: 'Imerzná čítačka',
+    sl: 'Potopni bralnik',
+    sq: 'Lexuesi kredhës',
+    'sr-cyrl-ba': 'Концентрисани читалац',
+    'sr-cyrl-rs': 'Концентрисани читалац',
+    'sr-latn-rs': 'Koncentrisani čitalac',
+    sv: 'Avancerad läsare',
+    sw: 'Kisomaji cha Kuzamisha',
+    ta: 'அற்புதமான ரீடர்',
+    te: 'మంత్రముగ్ధులను చేసే పఠన సాధనం',
+    'tg-cyrl-tj': 'Хонандаи фарогир',
+    th: 'โปรแกรมช่วยอ่าน',
+    ti: 'ኣንባቢ ሕሉው ስምዒት ፈጣሪ',
+    tk: 'Giňişleýin okaýjy',
+    tn: 'Sebadi sa Imesife',
+    tr: 'Tam Ekran Okuyucu',
+    tt: 'Чолгап алучы уку чарасы',
+    'ug-cn': 'چۆكمە ئوقۇغۇچ',
+    uk: 'Занурення в текст',
+    ur: 'امرسیو مطالعہ کار',
+    uz: 'Immersiv mutolaa vositasi',
+    vi: 'Trình đọc Chân thực',
+    wo: 'Jàngukaay bu Rafet',
+    xh: 'Isifundi Esikhulu',
+    yo: 'Ìwò Alámùtán',
+    zh: '沉浸式阅读器',
+    'zh-hant': '沈浸式閱讀程式',
+    zu: 'Isifundi Sokuzizwisa'
+};
+
+export function renderButtons(): void {
+    const iconElements = <HTMLCollectionOf<HTMLDivElement>>document.getElementsByClassName('immersive-reader-button');
+    for (const div of iconElements) {
+        div.style.cursor = 'pointer';
+        div.style.display = 'inline-block';
+        div.style.padding = '5px';
+
+        const style = div.getAttribute(attributeButtonStyle) || 'icon';
+        switch (style) {
+            case 'icon':
+                renderIcon(div);
+                break;
+            case 'text':
+                renderText(div);
+                break;
+            case 'iconAndText':
+                renderIcon(div);
+                renderMargin(div);
+                renderText(div);
+                break;
+        }
+    }
+}
+
+function renderIcon(div: HTMLDivElement) {
+    const image = document.createElement('img');
+    image.src = iconUrl;
+    const iconSize = div.getAttribute(attributeIconPxSize) || '20';
+    image.style.height = image.style.width = iconSize + 'px';
+    image.style.verticalAlign = 'middle';
+    image.style.marginTop = '-2px';
+    const locale = div.getAttribute(attributeLocale) || 'en';
+    image.alt = getLocalizedString(locale);
+    div.appendChild(image);
+}
+
+function renderMargin(div: HTMLDivElement) {
+    const span = document.createElement('span');
+    span.style.marginLeft = '8px';
+    div.appendChild(span);
+}
+
+function renderText(div: HTMLDivElement) {
+    const span = document.createElement('span');
+    const locale = div.getAttribute(attributeLocale) || 'en';
+    span.textContent = getLocalizedString(locale);
+    div.appendChild(span);
+}
+
+function getLocalizedString(locale: string) {
+    locale = locale.toLowerCase();
+
+    // Look for the localization, if the language is supported.
+    if (locs[locale]) {
+        return locs[locale];
+    }
+
+    // Special case handling for Chinese, in which certain locales should resolve to Traditional Chinese, and
+    // the others should resolve to Simplified Chinese.
+    if (locale === 'zh-hk' || locale === 'zh-mo' || locale === 'zh-tw') {
+        return locs['zh-hant'];
+    }
+
+    // Remove the subscript portion (or the region) portion of the locale, and check to see if the 
+    // localization exists for the resulting locale.
+    locale = locale.substring(0, locale.lastIndexOf('-'));
+    if (locs[locale]) {
+        return locs[locale];
+    }
+
+    // Remove the region portion of the locale, and check to see if the localization exists for the resulting locale.
+    locale = locale.substring(0, locale.lastIndexOf('-'));
+    if (locs[locale]) {
+        return locs[locale];
+    }
+
+    // If all else fails, show the string in English.
+    return locs['en'];
+}
---
+++ tests/renderButtons.test.ts
@@ -0,0 +1,123 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+import { renderButtons } from '../src/renderButtons';
+
+function expectIcon(button: HTMLDivElement) {
+    let imageCount = 0;
+    for (const child of button.children) {
+        if (child.tagName === 'IMG') {
+            imageCount++;
+        }
+    }
+    expect(imageCount).toBe(1);
+}
+
+function expectText(button: HTMLDivElement, text: string) {
+    let spanCount = 0;
+    for (const child of button.children) {
+        if (child.tagName === 'SPAN' && child.textContent) {
+            expect(child.textContent).toBe(text);
+            spanCount++;
+        }
+    }
+    expect(spanCount).toBe(1);
+}
+
+describe('renderButtons', () => {
+    it('styles a single icon button', () => {
+        const button: HTMLDivElement = document.createElement('div');
+        button.className = 'immersive-reader-button';
+        document.body.appendChild(button);
+
+        renderButtons();
+
+        expectIcon(button);
+
+        // Cleanup the DOM
+        button.remove();
+    });
+
+    it('styles a single icon+text button', () => {
+        const button: HTMLDivElement = document.createElement('div');
+        button.className = 'immersive-reader-button';
+        button.setAttribute('data-button-style', 'iconAndText');
+        document.body.appendChild(button);
+
+        renderButtons();
+
+        expectIcon(button);
+        expectText(button, 'Immersive Reader');
+
+        // Cleanup the DOM
+        button.remove();
+    });
+
+    it('styles a single text button', () => {
+        const button: HTMLDivElement = document.createElement('div');
+        button.className = 'immersive-reader-button';
+        button.setAttribute('data-button-style', 'text');
+        document.body.appendChild(button);
+
+        renderButtons();
+
+        expectText(button, 'Immersive Reader');
+
+        // Cleanup the DOM
+        button.remove();
+    });
+    
+    it('styles a single text button in a different language', () => {
+        const button: HTMLDivElement = document.createElement('div');
+        button.className = 'immersive-reader-button';
+        button.setAttribute('data-button-style', 'text');
+        button.setAttribute('data-locale', 'fr-FR');
+        document.body.appendChild(button);
+
+        renderButtons();
+
+        expectText(button, 'Lecteur immersif');
+
+        // Cleanup the DOM
+        button.remove();
+    });
+
+    it('styles multiple buttons', () => {
+        const button: HTMLDivElement = document.createElement('div');
+        const button2: HTMLDivElement = document.createElement('div');
+        button.className = 'immersive-reader-button';
+        button2.className = 'immersive-reader-button';
+        button2.setAttribute('data-button-style', 'iconAndText');
+        document.body.appendChild(button);
+        document.body.appendChild(button2);
+
+        renderButtons();
+
+        expectIcon(button);
+        expectIcon(button2);
+        expectText(button2, 'Immersive Reader');
+
+        // Cleanup the DOM
+        button.remove();
+        button2.remove();
+    });
+
+    it('does nothing for other buttons', () => {
+        const newDiv1: HTMLDivElement = document.createElement('div');
+        const newDiv2: HTMLDivElement = document.createElement('div');
+        newDiv1.className = 'button1';
+        newDiv2.className = 'something-else';
+
+        document.body.appendChild(newDiv1);
+        document.body.appendChild(newDiv2);
+
+        renderButtons();
+
+        expect(newDiv1.children.length).toBe(0);
+        expect(newDiv2.children.length).toBe(0);
+
+        // Cleanup the DOM
+        newDiv1.remove();
+        newDiv2.remove();
+    });
+});
