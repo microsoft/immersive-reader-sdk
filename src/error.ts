12e6da5c331617fb3a38bbfde7d02d08950533c7
Meng Tan <48224849+metanMSFT@users.noreply.github.com>
2019-06-14 14:38:59 -0700

    Return appropriate error if token expired

--- src/error.ts
+++ src/error.ts
@@ -2,6 +2,6 @@
 // Licensed under the MIT License.
 
 export type Error = {
-    code: string;    // One of a set of error codes (BadArgument, Timeout)
+    code: string;    // One of a set of error codes (BadArgument, Timeout, TokenExpired)
     message: string; // Human-readable representation of the error
 }
