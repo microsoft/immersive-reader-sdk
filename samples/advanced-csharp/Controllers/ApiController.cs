9e0df195791b5b897b53d125005a118adee3c910
Ryan Waller <rwaller@microsoft.com>
2019-07-11 11:21:50 -0700

    Update ReadmMe with simpler instructions, change tabs to spaces

--- samples/advanced-csharp/Controllers/ApiController.cs
+++ samples/advanced-csharp/Controllers/ApiController.cs
@@ -6,74 +6,74 @@ using System.Threading.Tasks;
 
 namespace AdvancedSampleWebApp.Pages
 {
-	[Route("api")]
-	[ApiController]
-	public class ApiController : ControllerBase
-	{
-		private readonly string TenantId;     // Azure subscription TenantId
-		private readonly string ClientId;     // AAD ApplicationId
-		private readonly string ClientSecret; // AAD Application Service Principal password
+    [Route("api")]
+    [ApiController]
+    public class ApiController : ControllerBase
+    {
+        private readonly string TenantId;     // Azure subscription TenantId
+        private readonly string ClientId;     // AAD ApplicationId
+        private readonly string ClientSecret; // AAD Application Service Principal password
 
-		public ApiController(Microsoft.Extensions.Configuration.IConfiguration configuration)
-		{
-			TenantId = configuration["TenantId"];
-			ClientId = configuration["ClientId"];
-			ClientSecret = configuration["ClientSecret"];
+        public ApiController(Microsoft.Extensions.Configuration.IConfiguration configuration)
+        {
+            TenantId = configuration["TenantId"];
+            ClientId = configuration["ClientId"];
+            ClientSecret = configuration["ClientSecret"];
 
-			if (string.IsNullOrWhiteSpace(TenantId))
-			{
-				throw new ArgumentNullException("TenantId is null! Did you add that info to secrets.json? See ReadMe.txt.");
-			}
+            if (string.IsNullOrWhiteSpace(TenantId))
+            {
+                throw new ArgumentNullException("TenantId is null! Did you add that info to secrets.json? See ReadMe.txt.");
+            }
 
-			if (string.IsNullOrWhiteSpace(ClientId))
-			{
-				throw new ArgumentNullException("ClientId is null! Did you add that info to secrets.json? See ReadMe.txt.");
-			}
+            if (string.IsNullOrWhiteSpace(ClientId))
+            {
+                throw new ArgumentNullException("ClientId is null! Did you add that info to secrets.json? See ReadMe.txt.");
+            }
 
-			if (string.IsNullOrWhiteSpace(ClientSecret))
-			{
-				throw new ArgumentNullException("ClientSecret is null! Did you add that info to secrets.json? See ReadMe.txt.");
-			}
-		}
+            if (string.IsNullOrWhiteSpace(ClientSecret))
+            {
+                throw new ArgumentNullException("ClientSecret is null! Did you add that info to secrets.json? See ReadMe.txt.");
+            }
+        }
 
-		[Route("token")]
-		[HttpPost]
-		[Consumes("text/plain")]
-		[Produces("text/plain")]
-		public async Task<string> Token()
-		{
-			// Retrieve the canary value and authenticate
-			string canary;
-			using (StreamReader reader = new StreamReader(Request.Body))
-			{
-				canary = reader.ReadToEnd();
-			}
+        [Route("token")]
+        [HttpPost]
+        [Consumes("text/plain")]
+        [Produces("text/plain")]
+        public async Task<string> Token()
+        {
+            // Retrieve the canary value and authenticate
+            string canary;
+            using (StreamReader reader = new StreamReader(Request.Body))
+            {
+                canary = reader.ReadToEnd();
+            }
 
-			if (string.IsNullOrEmpty(canary))
-			{
-				throw new Exception("Canary missing");
-			}
+            if (string.IsNullOrEmpty(canary))
+            {
+                throw new Exception("Canary missing");
+            }
 
-			if (!Canary.Validate(canary))
-			{
-				throw new Exception("Authentication failed");
-			}
+            if (!Canary.Validate(canary))
+            {
+                throw new Exception("Authentication failed");
+            }
 
-			// Obtain a token using the subscription key
-			return await GetTokenAsync();
-		}
+            // Obtain a token using the subscription key
+            return await GetTokenAsync();
+        }
 
-		protected async Task<string> GetTokenAsync()
-		{
-			string authority = $"https://login.windows-ppe.net/{TenantId}";
-			const string resource = "https://ppe.cognitiveservices.azure.com/";
+        protected async Task<string> GetTokenAsync()
+        {
+            string authority = $"https://login.windows-ppe.net/{TenantId}";
+            const string resource = "https://ppe.cognitiveservices.azure.com/";
 
-			AuthenticationContext authContext = new AuthenticationContext(authority);
-			ClientCredential clientCredential = new ClientCredential(ClientId, ClientSecret);
+            AuthenticationContext authContext = new AuthenticationContext(authority);
+            ClientCredential clientCredential = new ClientCredential(ClientId, ClientSecret);
 
-			AuthenticationResult authResult = await authContext.AcquireTokenAsync(resource, clientCredential);
+            AuthenticationResult authResult = await authContext.AcquireTokenAsync(resource, clientCredential);
 
-			return authResult.AccessToken;
-		}
-	}
+            return authResult.AccessToken;
+        }
+    }
 }
--- samples/quickstart-csharp/ReadMe.txt
+++ samples/quickstart-csharp/ReadMe.txt
@@ -3,23 +3,11 @@
 
 Follow these steps to get the sample up and running:
 
-1. Create an Immersive Reader cognitive service resource in Azure with a custom Subdomain
+1. Create an Immersive Reader cognitive service resource in Azure with a custom Subdomain.
 
-    There are 2 methods to create the resource. Each of these methods has a different way of producing a 'Subdomain' name that is used to make calls to the service. You will need to provide this 'Subdomain' in the sample and when making calls to the SDK.
+   Azure portal - https://portal.azure.com/#create/Microsoft.CognitiveServicesImmersiveReader
 
-        1) Azure portal - https://portal.azure.com/#create/Microsoft.CognitiveServicesImmersiveReader
-
-           When using the Azure portal to create the Immersive Reader cognitive service resource, it will ask you to provide a 'Name' for the resource. This resource name becomes the 'Subdomain' name behind the scenes.
-       
-        2) Powershell/Azure Cloud Shell:
-
-           See https://docs.microsoft.com/en-us/azure/cognitive-services/authentication#create-a-resource-with-a-custom-subdomain for details on how to create the resource.
-
-               Example:
-
-                   >New-AzCognitiveServicesAccount -ResourceGroupName <RESOURCE_GROUP_NAME> -nNme <ACCOUNT_NAME> -Type ImmersiveReader -SkuName S0 -Location <REGION> -CustomSubdomainName <UNIQUE_SUBDOMAIN>
-      
-           With Powershell you are able to specify the 'CustomSubdomainName' separately from the 'Name'.
+   When using the Azure portal to create the Immersive Reader cognitive service resource, it will ask you to provide a 'Name' for the resource. This resource name becomes the 'Subdomain' name behind the scenes. You will need to provide this 'Subdomain' in the sample and when making calls to the SDK.
 
 
 2. Follow the instructions at https://docs.microsoft.com/en-us/azure/cognitive-services/authentication?#authenticate-with-azure-active-directory to get Cognitive Service AAD Authentication set up in Azure. 
@@ -31,7 +19,7 @@ Follow these steps to get the sample up and running:
        3) Assigning the 'Cognitive Services User' role to the service principal, granting it access to the Immersive Reader resource created above (via the -scope option)
 
 
-3. Open QuickStartSampleWebApp.sln in Visual Studio
+3. Open AdvancedSampleWebApp.sln in Visual Studio.
 
 
 4. Right-click on the project in the Solution Explorer and choose "Manage User Secrets". This will open a file called secrets.json. Replace the contents of that file with the following, supplying your custom property values as appropriate:
@@ -54,7 +42,7 @@ Follow these steps to get the sample up and running:
     Subdomain    => Immersive Reader cognitive service resource subdomain (resource 'Name' if the resource was created in the Azure portal, or 'CustomSubDomain' option if the resource was created in Powershell - check the Azure portal for the subdomain on the Endpoint in the resource Overview page, e.g. it will look like 'https://[SUBDOMAIN].cognitiveservices.azure.com/')
 
 
-5. Run Debug->Start Debugging
+5. Run Debug->Start Debugging.
 
 
 License
