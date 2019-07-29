6e7ac3b52da2abc33d90b45719d2ca55b45bf12f
Meng Tan <48224849+metanMSFT@users.noreply.github.com>
2019-07-29 10:35:41 -0700

    Add support for local mode

--- build/edog.js
+++ build/edog.js
@@ -7,9 +7,22 @@ const fs = require('fs');
 const replace = require('replace-in-file');
 
 fs.copyFileSync('lib/immersive-reader-sdk.js', 'lib/immersive-reader-sdk.edog.js');
+fs.copyFileSync('lib/immersive-reader-sdk.js', 'lib/immersive-reader-sdk.local.js');
 
 replace.sync({
     files: 'lib/immersive-reader-sdk.edog.js',
     from: /learningtools\.onenote\.com/g,
     to: 'learningtools.edog.onenote.com'
+});
+
+replace.sync({
+    files: 'lib/immersive-reader-sdk.local.js',
+    from: /https:\/\/learningtools\.onenote\.com\/learningtoolsapp\/cognitive\/reader/g,
+    to: 'reader.html'
+});
+
+replace.sync({
+    files: 'lib/immersive-reader-sdk.local.js',
+    from: /https:\/\/contentstorage\.onenote\.office\.net\/onenoteltir\/permanent-static-resources\//g,
+    to: ''
 });
