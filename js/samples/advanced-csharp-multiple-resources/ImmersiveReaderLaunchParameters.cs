d393d23fe143991b7996ac81695eba28c2996ea7
pastakof <pastakof@microsoft.com>
2019-07-29 17:59:03 -0700

    Add multiple resources sample

---
+++ js/samples/advanced-csharp-multiple-resources/ImmersiveReaderLaunchParameters.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MultipleResourcesSampleWebApp
+{
+	/// <summary>
+	/// Parameters used to launch the Immersive Reader on the client side
+	/// </summary>
+	public struct ImmersiveReaderLaunchParameters
+	{
+		public string Token { get; set; }
+		public string Subdomain { get; set; }
+	}
+}
---
+++ js/samples/advanced-csharp-multiple-resources/ImmersiveReaderResourceConfig.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MultipleResourcesSampleWebApp
+{
+	/// <summary>
+	/// Configuration values for a resource created with Azure Active Directory
+	/// https://docs.microsoft.com/en-us/azure/cognitive-services/immersive-reader/azure-active-directory-authentication
+	/// </summary>
+	public struct ImmersiveReaderResourceConfig
+	{
+		public string TenantId { get; set; } // Azure subscription TenantId
+		public string ClientId { get; set; } // Azure AD ApplicationId
+		public string ClientSecret { get; set; } // Azure AD Application Service Principal password
+		public string Subdomain { get; set; } // Immersive Reader resource subdomain (resource 'Name' if the resource was created in the Azure portal, or 'CustomSubDomain' option if the resource was created with Azure CLI Powershell. Check the Azure portal for the subdomain on the Endpoint in the resource Overview page, for example, 'https://[SUBDOMAIN].cognitiveservices.azure.com/')
+	}
+}
