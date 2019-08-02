e19edba63f73a3f2d9d82eccf5bd7d7ab55a972c
Megan <31525860+MeganRoach@users.noreply.github.com>
2019-08-01 18:52:39 -0700

    Add Picture to Immersive Reader Sample

---
+++ iOS/samples/picture-to-immersive-reader-swift/picture-to-immersive-reader-swift/Constants.swift
@@ -0,0 +1,16 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+import Foundation
+
+// This file contains secrets. Do not check this file into source control. Keep this file as a local file that only exists on your development machine.
+
+public struct Constants {
+    static let tenantId: String = "<YOUR_TENANT_ID>"
+    static let clientId: String = "<YOUR_CLIENT_ID>"
+    static let clientSecret: String = "<YOUR_CLIENT_SECRET>"
+    static let subdomain: String = "<YOUR_SUBDOMAIN>"
+    
+    static let computerVisionSubscriptionKey: String = "<YOUR_SUBSCRIPTION_KEY>"
+    static let computerVisionEndPoint: String = "<YOU_ENDPOINT>"
+}
