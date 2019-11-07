fa0ea20dc5d34b8ff2f69f323be8a84b86e0d108
Such Kamal <skamal@microsoft.com>
2019-11-07 13:04:02 -0800

    Remove (probably) outdated compatibility code

--- js/src/immersive-reader-sdk.ts
+++ js/src/immersive-reader-sdk.ts
@@ -13,26 +13,10 @@ window.addEventListener('load', () => {
 });
 
 function dynamicallyLoadScript(scriptUrl: string) {
-    const head = document.getElementsByTagName('head')[0];
     const script = document.createElement('script');
     script.src = scriptUrl;
     script.async = true;
-    let done = false;
-    script.onload = (script as any).onreadystatechange = function () {
-        // 'this' refers to the XMLHttpRequest scope, readyState is a property on 'this'
-        // tslint:disable-next-line:no-invalid-this
-        if (!done && (!this.readyState || this.readyState === 'loaded' || this.readyState === 'complete')) {
-            done = true;
-
-            // Handle memory leak in IE
-            script.onload = (script as any).onreadystatechange = null;
-            if (head && script.parentNode) {
-                head.removeChild(script);
-            }
-        }
-    };
-
-    head.appendChild(script);
+    document.head.appendChild(script);
 }
 
 export { renderButtons, close, launchAsync };
