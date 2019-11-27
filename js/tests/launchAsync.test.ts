8a325eaf4868447bbf9e4190e18cc30b7e6ad4af
Ryan Waller <rwaller@microsoft.com>
2019-11-27 15:14:29 -0800

    Adjust Timeout to apply to Reader load only and adjust error behavior to Exit and call OnExit callback for ALL server-side call/procesing errors

--- js/tests/launchAsync.test.ts
+++ js/tests/launchAsync.test.ts
@@ -144,30 +144,51 @@ describe('launchAsync tests', () => {
         const iframe = <HTMLIFrameElement>container.firstElementChild;
         expect(iframe.src.toLowerCase()).toMatch('https://foo.com/');
     });
+});
 
-    describe('Utility method isValidSubdomain', () => {
-        it('should return false', () => {
-            expect(isValidSubdomain(null)).toBe(false);
-            expect(isValidSubdomain(undefined)).toBe(false);
-            expect(isValidSubdomain('')).toBe(false);
-            expect(isValidSubdomain('é')).toBe(false);
-            expect(isValidSubdomain('hasaccént')).toBe(false);
-            expect(isValidSubdomain('1é2')).toBe(false);
-            expect(isValidSubdomain('É')).toBe(false);
-            expect(isValidSubdomain('Ã')).toBe(false);
-            expect(isValidSubdomain('has space')).toBe(false);
-            expect(isValidSubdomain('has.period')).toBe(false);
-            expect(isValidSubdomain(' startswithspace')).toBe(false);
-            expect(isValidSubdomain('endswithspace ')).toBe(false);
-            expect(isValidSubdomain('-startswithdash')).toBe(false);
-            expect(isValidSubdomain('endswithdash-')).toBe(false);
-        });
-
-        it('should return true', () => {
-            expect(isValidSubdomain('valid')).toBe(true);
-            expect(isValidSubdomain('valid10with2numbers')).toBe(true);
-            expect(isValidSubdomain('1234')).toBe(true);
-        });
+describe('Utility method isValidSubdomain', () => {
+    it('should return false', () => {
+        expect(isValidSubdomain(null)).toBe(false);
+        expect(isValidSubdomain(undefined)).toBe(false);
+        expect(isValidSubdomain('')).toBe(false);
+        expect(isValidSubdomain('é')).toBe(false);
+        expect(isValidSubdomain('hasaccént')).toBe(false);
+        expect(isValidSubdomain('1é2')).toBe(false);
+        expect(isValidSubdomain('É')).toBe(false);
+        expect(isValidSubdomain('Ã')).toBe(false);
+        expect(isValidSubdomain('has space')).toBe(false);
+        expect(isValidSubdomain('has.period')).toBe(false);
+        expect(isValidSubdomain(' startswithspace')).toBe(false);
+        expect(isValidSubdomain('endswithspace ')).toBe(false);
+        expect(isValidSubdomain('-startswithdash')).toBe(false);
+        expect(isValidSubdomain('endswithdash-')).toBe(false);
     });
 
+    it('should return true', () => {
+        expect(isValidSubdomain('valid')).toBe(true);
+        expect(isValidSubdomain('valid10with2numbers')).toBe(true);
+        expect(isValidSubdomain('1234')).toBe(true);
+    });
 });
+
+const fs = require('fs');
+describe('Verify SDK version is valid', () => {
+
+    it('check version', () => {
+        const packageJson: string = fs.readFileSync("package.json", "utf8");
+        const sdkVersion: string = JSON.parse(packageJson).version;
+        console.log(`SDK version: ${sdkVersion}`);
+
+        expect(isValidSDKVersion(sdkVersion)).toBe(true);
+    });
+});
+
+// sdk version must be in format xxx.xxx.xxx (each version segment between 1 and 3 digits)
+function isValidSDKVersion(sdkVersion: string): boolean {
+    if (!sdkVersion) {
+        return false;
+    }
+
+    const regExp = /^[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}$/;
+    return regExp.test(sdkVersion);
+}
