3dd3cd42d7f151331d750be5b3bf018b46f4d2af
Meng Tan <48224849+metanMSFT@users.noreply.github.com>
2020-01-09 07:22:32 -0800

    Fix button styling and also fix up Readme.md (#147)
    * Remove type=button from renderButtons
    
    * Fix Readme

--- js/samples/ios/quickstart-swift/Resources/iFrameMessaging.js
+++ js/samples/ios/quickstart-swift/Resources/iFrameMessaging.js
@@ -2,25 +2,23 @@
 // Licensed under the MIT License.
 
 window.addEventListener("message", function(message) {
-   
     if(message.data == "ImmersiveReader-ReadyForContent") {
         window.webkit.messageHandlers.readyForContent.postMessage(null);
     }
-    
+
     if(message.data == "ImmersiveReader-LaunchSuccessful") {
         window.webkit.messageHandlers.launchSuccessful.postMessage(null);
     }
-    
+
     if(message.data == "ImmersiveReader-TokenExpired") {
         window.webkit.messageHandlers.tokenExpired.postMessage(null);
     }
-    
+
     if(message.data == "ImmersiveReader-Throttled") {
         window.webkit.messageHandlers.throttled.postMessage(null);
     }
 });
 
 function sendContentToReader(message) {
-    // Send a post message to the iframe.
-    document.getElementById('immersiveReaderIframe').contentWindow.postMessage(JSON.stringify({'messageType':'Content', 'messageValue': message}), '*');
+    document.getElementById('immersiveReaderIframe').contentWindow.postMessage(JSON.stringify({messageType:'Content', messageValue: message}), '*');
 }
