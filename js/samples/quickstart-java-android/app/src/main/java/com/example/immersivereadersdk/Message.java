159aadb884856be0889e1ddc2845c9e6f8a9fe95
dylankil <dylankil@microsoft.com>
2020-09-16 14:43:57 -0700

    Updates the quickstart-java-android code sample to use the v0.0.3 JavaScript SDK and the v0.0.1 authentication token. Updates README

--- js/samples/quickstart-java-android/app/src/main/java/com/example/immersivereadersdk/Message.java
+++ js/samples/quickstart-java-android/app/src/main/java/com/example/immersivereadersdk/Message.java
@@ -6,14 +6,12 @@ import androidx.annotation.Keep;
 public class Message {
 
     public String cogSvcsAccessToken;
-    public String cogSvcsSubdomain;
     public Content request;
     public Integer launchToPostMessageSentDurationInMs;
     public Options options;
 
-    public Message(String cogSvcsAccessToken, String cogSvcsSubdomain, Content request, Integer launchToPostMessageSentDurationInMs, Options options) {
+    public Message(String cogSvcsAccessToken, Content request, Integer launchToPostMessageSentDurationInMs, Options options) {
         this.cogSvcsAccessToken = cogSvcsAccessToken;
-        this.cogSvcsSubdomain = cogSvcsSubdomain;
         this.request = request;
         this.launchToPostMessageSentDurationInMs = launchToPostMessageSentDurationInMs;
         this.options = options;
