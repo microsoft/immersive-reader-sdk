97faea8dbbbc21f8ee2b140b718107e7e797ea61
dylankil <dylankil@microsoft.com>
2020-09-16 10:34:22 -0700

    Applies backfix to quickstart-nodejs code sample to use v0.0.3 Immersive Reader JavaScript SDK and the v0.0.1 authentication token. Updates README

--- js/samples/quickstart-nodejs/routes/index.js
+++ js/samples/quickstart-nodejs/routes/index.js
@@ -7,41 +7,21 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
-router.get('/GetTokenAndSubdomain', function(req, res) {
-    try {
-        request.post({
+router.get('/GetToken', function(req, res) {
+    request.post({
             headers: {
+                'Ocp-Apim-Subscription-Key': process.env.SUBSCRIPTION_KEY,
                 'content-type': 'application/x-www-form-urlencoded'
             },
-            url: `https://login.windows.net/${process.env.TENANT_ID}/oauth2/token`,
-            form: {
-                grant_type: 'client_credentials',
-                client_id: process.env.CLIENT_ID,
-                client_secret: process.env.CLIENT_SECRET,
-                resource: 'https://cognitiveservices.azure.com/'
-            }
+            url: 'https://' + process.env.REGION + '.api.cognitive.microsoft.com/sts/v1.0/issueToken'
         },
-        function(err, resp, tokenResult) {
+        function(err, resp, token) {
             if (err) {
-                console.log(err);
                 return res.status(500).send('CogSvcs IssueToken error');
             }
-    
-            var tokenResultParsed = JSON.parse(tokenResult);
-            
-            if (tokenResultParsed.error) {
-                console.log(tokenResult);
-                return res.send({error :  "Unable to acquire Azure AD token. Check the debugger for more information."})
-            }
-    
-            var token = tokenResultParsed.access_token;
-            var subdomain = process.env.SUBDOMAIN;
-            return res.send({token, subdomain});
-        });
-    } catch (err) {
-        console.log(err);
-        return res.status(500).send('CogSvcs IssueToken error');
-    }
+
+            return res.send(token);
+    });
 });
 
 module.exports = router;
