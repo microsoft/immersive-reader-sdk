7a7341e87ca12b13ac17731d8be120308b4d810e
Dylan Kilgore <dylankil@microsoft.com>
2021-12-13 16:21:52 -0800

    Replaces wildcard with generic localhost target origin.

--- js/samples/ios/quickstart-swift/Resources/iFrameMessaging.js
+++ js/samples/ios/quickstart-swift/Resources/iFrameMessaging.js
@@ -16,6 +16,8 @@ window.addEventListener("message", function(message) {
     }
 });
 
+// IMPORTANT: When posting a message to an iframe, replace the below target with your localhost and port.
+// https://developer.mozilla.org/en-US/docs/Web/API/Window/postMessage
 function sendContentToReader(message) {
-    document.getElementById('immersiveReaderIframe').contentWindow.postMessage(JSON.stringify({messageType:'Content', messageValue: message}), '*');
+    document.getElementById('immersiveReaderIframe').contentWindow.postMessage(JSON.stringify({messageType:'Content', messageValue: message}), 'http://127.0.0.1:8080');
 }
