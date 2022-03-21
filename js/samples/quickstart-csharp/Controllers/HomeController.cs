7dc72583ae293d2e3d2049ab435bf7e8ba38a6ce
Cristobal Buenrostro (ZEN3 INFOSOLUTIONS AMERICA INC) <v-cristobalb@microsoft.com>
2022-03-21 16:43:04 -0700

    Port latest SDK options - Csharp

--- js/samples/quickstart-csharp/Controllers/HomeController.cs
+++ js/samples/quickstart-csharp/Controllers/HomeController.cs
@@ -97,6 +97,11 @@ namespace QuickstartSampleWebApp.Controllers
             return View();
         }
 
+        public IActionResult Options()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
