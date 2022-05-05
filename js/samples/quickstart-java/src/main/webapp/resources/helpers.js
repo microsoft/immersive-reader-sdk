a637e3b98b850b20134d8d6f48446da3422bb24f
cristobal-buenrostro <87383938+cristobal-buenrostro@users.noreply.github.com>
2022-05-05 10:33:27 -0700

    Port latest SDK options - Java (#359)
    * Port latest SDK options - Java
    
    * move console log
    
    * renaming get token fn

---
+++ js/samples/quickstart-java/src/main/webapp/resources/helpers.js
@@ -0,0 +1,21 @@
+function getTokenAsync() {
+    return new Promise(function (resolve, reject) {
+        $.ajax({
+            url: '/getAuthTokenServlet',
+            type: 'GET',
+            success: function (response) {
+                const data = JSON.parse(response);
+
+                if (data.error) {
+                    reject(data.error);
+                } else {
+                    const token = data['access_token'];
+                    resolve({token});
+                }
+            },
+            error: function (err) {
+                reject(err);
+            }
+        });
+    });
+}
