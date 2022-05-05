4f1a373179e3d2e09a6a58d687aab02305e4a33f
cristobal-buenrostro <87383938+cristobal-buenrostro@users.noreply.github.com>
2022-05-05 10:33:01 -0700

    Port latest SDK options - Node JS (#354)
    * Port latest SDK options - Node JS
    
    * change alert and console log order
    
    * move console log

--- js/samples/quickstart-nodejs/app.js
+++ js/samples/quickstart-nodejs/app.js
@@ -8,6 +8,7 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var optionsRouter = require('./routes/options');
 
 var app = express();
 
@@ -23,6 +24,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/options', optionsRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
