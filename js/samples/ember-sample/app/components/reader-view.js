1d9f7ff4d36006e655bb967b7ab0f0188c80ba04
cristobal-buenrostro <87383938+cristobal-buenrostro@users.noreply.github.com>
2022-05-05 10:34:25 -0700

    Port latest SDK options - Ember (#365)
    * Port latest SDK options - Ember
    
    * remove IF and mode console.log prior to alerts

--- js/samples/ember-sample/app/components/reader-view.js
+++ js/samples/ember-sample/app/components/reader-view.js
@@ -20,8 +20,8 @@ export default class ReaderViewComponent extends Component {
             this.subdomain = subdomain;
         }
         catch (err) {
-            alert('There was a problem fetching your credentials, please check the console and make sure your environment variables are set');
             console.log({ err })
+            alert('There was a problem fetching your credentials, please check the console and make sure your environment variables are set');
         }
     }
 
@@ -47,16 +47,42 @@ export default class ReaderViewComponent extends Component {
             "onExit": this.exitCallback
         };
 
-        if (this.token === null) {
-            alert('Error please first retrieve your token!')
-        } else {
-            try {
-                await launchAsync(this.token, this.subdomain, data, options)
-            }
-            catch (error) {
-                alert("Error in launching the Immersive Reader. Check the console.");
-                console.log(error);
-            }
+        try {
+            await launchAsync(this.token, this.subdomain, data, options)
+        }
+        catch (error) {
+            console.log(error);
+            alert("Error in launching the Immersive Reader. Check the console.");
+        }
+    }
+
+    @action
+    async handleLaunchImmersiveReader(sampleId) {
+        await this.getTokenAndSubdomainAsync();
+        var langElement = $('#lang_' + sampleId);
+        var lang = sampleId == 'DisableLanguageDetection' ? langElement.val() : 'en';
+
+        const data = {
+            title: $('#title' + sampleId).text().trim(),
+            chunks: [{
+                content: $('#text' + sampleId).text().trim(),
+                lang: lang
+            }]
+        };
+
+        let options = {};
+
+        options.disableLanguageDetection = sampleId === 'DisableLanguageDetection';
+        options.disableGrammar = sampleId === 'DisableGrammar';
+        options.disableTranslation = sampleId === 'DisableTranslation';
+        options.parent = sampleId === 'Parent' && document.getElementById("checkboxParent").checked ? document.getElementById('parentDiv') : null;
+
+        try{
+            await launchAsync(this.token, this.subdomain, data, options);
+        }
+        catch (error) {
+            console.log(error);
+            alert("Error in launching the Immersive Reader. Check the console.");
         }
     }
 }
