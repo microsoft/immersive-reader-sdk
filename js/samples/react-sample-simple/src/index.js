70f0f61bfcc14ece810fad20a4b092c09c2a52c6
cristobal-buenrostro <87383938+cristobal-buenrostro@users.noreply.github.com>
2022-05-10 06:54:36 -0700

    Port latest SDK options - React (#357)
    * Port latest SDK options - React
    
    * validating env variables and moving console logs
    
    * validate falsy for env vars

--- js/samples/react-sample-simple/src/index.js
+++ js/samples/react-sample-simple/src/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import AppOptions from './AppOptions';
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
+    <AppOptions />
   </React.StrictMode>,
   document.getElementById('root')
 );
